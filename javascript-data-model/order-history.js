78a6276ad1296b62f2e2b836eb47a7134eb4e60c
Daniel Perez <danielperezf98@gmail.com>
2023-08-29 17:46:33 +0000

    Javascript Data Model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,52 @@
+const orderHistory = [
+  {
+    orderDate: 'August 4, 2020',
+    orderNum: '114-3941689-8772232',
+    shipTo: 'JS Masher',
+    totalItems: 1,
+    item: 'Javascript for impatient programers by Rauschmayer, Dr Axel',
+    price: 31.55,
+    shippedPrice: 34.0,
+    deliveryDate: 'August 8, 2020',
+    returnWindow: 'Sep 7, 2020',
+  },
+
+  {
+    orerDate: 'July 19, 2020',
+    orderNum: '113-9984268-1280257',
+    shipTo: 'JS Masher',
+    totalItems: 1,
+    item: 'The Timeless Way of Building by Alexander, Christopher',
+    price: 41.33,
+    shippedPrice: 44.53,
+    deliveryDate: 'July 20, 2020',
+    returnWindow: 'August 19, 2020',
+  },
+
+  {
+    orderDate: 'July 4, 2020',
+    orderNum: '114-2875557-9059409',
+    shipTo: 'JS Masher',
+    totalItems: 1,
+    item: 'Gamecube Controller Adapter.Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    price: 15.98,
+    shippedPrice: 17.22,
+    deliveryDate: 'July 7, 2020',
+    returnWindow: 'August 5,2020',
+  },
+
+  {
+    orderDate: 'July 3, 2020',
+    orderNum: '113-2883177-2648248',
+    shipTo: 'JS Masher',
+    totalItems: 2,
+    item1: 'GameCube Controller - Super Smash Bros. Edition (Ninentndo Switch)',
+    item1Price: 94.95,
+    item2: 'The Art of Soul by Faroult, Stephane',
+    item2Price: 33.99,
+    shippedPrice: 138.93,
+    deliveryDate: 'July 5, 2020',
+    returnWindow: 'August 4, 2020',
+  },
+];
+console.log(orderHistory);
