89b4a5c70b6488dfcc5a3af9ccb489a99f7b1d1c
Daniel Perez <danielperezf98@gmail.com>
2023-10-26 02:38:39 +0000

    SQL Aggregates

---
+++ sql-aggregates/movie-maniacs.sql
@@ -0,0 +1,6 @@
+select "firstName", "lastName",
+sum ("p"."amount") as "totalSpent"
+from "customers"
+join "payments" as "p" using ("customerId")
+group by "customerId"
+order by "totalSpent" desc;
