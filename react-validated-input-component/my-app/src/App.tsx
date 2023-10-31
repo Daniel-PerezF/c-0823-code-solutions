99150b678ffd100beb0b9baeed410a124b31b47b
Daniel Perez <danielperezf98@gmail.com>
2023-10-31 02:30:52 +0000

    React Validated Input Component

--- react-validated-input-component/my-app/src/App.tsx
+++ react-validated-input-component/my-app/src/App.tsx
@@ -2,11 +2,7 @@ import './App.css';
 import { ValidatedInput } from './ValidatedInput';
 
 function App() {
-  return (
-    <>
-      <ValidatedInput />
-    </>
-  );
+  return <ValidatedInput />;
 }
 
 export default App;
