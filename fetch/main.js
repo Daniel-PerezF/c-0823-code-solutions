331e1a2827f76746123f728072864cda15e75d1b
Daniel Perez <danielperezf98@gmail.com>
2023-11-06 17:59:35 +0000

    Fetch

--- fetch/main.js
+++ fetch/main.js
@@ -1,10 +1,13 @@
 async function logUsers() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) {
+      throw new Error('Error:', response.status);
+    }
     const users = await response.json();
     console.log(users);
   } catch (err) {
-    console.error('Error:', err);
+    console.error('Error:', err.message);
   }
 }
 logUsers();
@@ -12,10 +15,13 @@ logUsers();
 async function getPokemon() {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/3');
+    if (!response.ok) {
+      throw new Error('Error:', response.status);
+    }
     const pokemon = await response.json();
     console.log(pokemon);
   } catch (err) {
-    console.error('Error:', err);
+    console.error('Error:', err.message);
   }
 }
 getPokemon();
